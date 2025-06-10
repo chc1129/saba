5670eff6d4f3ea15b9f4864fbcc9f8c87ec29a64
chc1129 <chc1129@gmail.com>
2025-06-11 01:35:39 +0900

    code fix

--- saba_core/src/url.rs
+++ saba_core/src/url.rs
@@ -20,4 +20,46 @@ impl Url {
             searchpart: "".string(),
         }
     }
+
+    fn is_http(&self) -> bool {
+        if self.url.contains("http://") {
+            return true;
+        }
+        false
+    }
+
+    fn extract_host(&self) -> String {
+        let url_parts: Vec<&str> = self
+            .url
+            .trim_start_matches("http://")
+            .splitn(2, "/")
+            .collect();
+
+        if let Some(index) = url_parts[0].find(':') {
+            url_parts[0][..index].to_string()
+        } else {
+            url_parts[0].to_string()
+        }
+    }
+
+    fn extract_port(&self) -> String {
+        let url_parts: Vec<&str> = self
+            .url
+            .trim_start_matches("http://")
+            .splitn(2, "/")
+            .collect();
+
+        if let Some(index) = url_parts[0].find(':') {
+            url_parts[0][index + 1..].to_string()
+        } else {
+            "80".to_string()
+        }
+    }
+
+    pub fn parse(&mut self) -> Result<Self, String> {
+        if !self.is_http() {
+            return Err("Only HTTP scheme is supported.".to_string());
+        }
+    }
+
 }
