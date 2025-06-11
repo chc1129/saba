fa12ca422e9a10d600d215b91dec34d8366ee900
chc1129 <chc1129@gmail.com>
2025-06-12 01:16:17 +0900

    code fix

--- saba_core/src/url.rs
+++ saba_core/src/url.rs
@@ -21,6 +21,22 @@ impl Url {
         }
     }
 
+    pub fn host(&self) -> String {
+        self.host.clone();
+    }
+
+    pub fn port(&self) -> String {
+        self.port.clone();
+    }
+
+    pub fn path(&self) -> String {
+        self.path.clone();
+    }
+
+    pub fn searchpart(&self) -> String {
+        self.searchpart.clone()
+    }
+
     fn is_http(&self) -> bool {
         if self.url.contains("http://") {
             return true;
@@ -42,6 +58,21 @@ impl Url {
         }
     }
 
+    fn extract_poth(&self) -> String {
+        let url_parts: Vec<&str> = self
+            .url
+            .trim_start_matches("http://")
+            .splitn(2, "/")
+            .collect();
+
+        if url_pats.len() < 2 {
+            return "".to_string();
+        }
+
+        let path_and_searchpart: Vec<&str> = url_parts[1].splitn(2, "?").collect();
+        path_and_searchpart[0].to_string() // (d3)
+    }
+
     fn extract_port(&self) -> String {
         let url_parts: Vec<&str> = self
             .url
@@ -56,10 +87,37 @@ impl Url {
         }
     }
 
+    fn extract_searchpart(&self) -> String {
+        let url_parts: Vec<&str> = self
+            .url
+            .trim_start_matches("http://")
+            .splitn(2, "/")
+            .collect();
+
+        if url_parts.len() < 2 {
+            return "".to_string();
+        }
+
+        let path_and_searchpart: Vec<&str> = url_parts[1].splitn(2, "?").collect();
+        if path_and_searchpart.len() < 2 {
+            "".to_string()
+        } else {
+            path_and_searchpart[1].to_string()
+        }
+    }
+
+
     pub fn parse(&mut self) -> Result<Self, String> {
         if !self.is_http() {
             return Err("Only HTTP scheme is supported.".to_string());
         }
+
+        self.host = self.extract_host();
+        self.port = self.extract_port();
+        self.path = self.extract_path();
+        self.searchpart = self.extract_searchpart();
+
+        Ok(self.clone())
     }
 
 }
