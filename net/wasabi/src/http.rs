dab09e5bd20d17db7fc60cc9bb6908c12a878fef
chc1129 <chc1129@gmail.com>
2025-06-26 01:28:53 +0900

    code fix

--- net/wasabi/src/http.rs
+++ net/wasabi/src/http.rs
@@ -1,5 +1,7 @@
 extern crate alloc;
 use alloc::string::String;
+use alloc::string::ToString;
+use alloc::vec::Vec;
 use saba_core::error::Error;
 use saba_core::http::HttpResponse;
 
@@ -56,5 +58,27 @@ impl HttpClient {
         ))
       }
     };
+
+    let mut received = Vec::new();
+    loop {
+      let mut buf = [0u8; 4096];
+      let bytes_read = match stream.read(&mut buf) {
+        Ok(bytes) => bytes,
+        Err(_) => {
+          return Err(Error::Network(
+            "Failed to receive a request from TCP stream".to_string(),
+          ))
+        }
+      };
+      if bytes_read == 0 {
+        break;
+      }
+      received.extend_from_slice(&buf[..bytes_read]);
+    }
+
+    match core::str::from_utf8(&received) {
+      Ok(response) => HttpResponse::new(response.to_string()),
+      Err(e) => Err(Error::Network(format!("Invalid received response: {}", e))),
+    }
   }
 }
