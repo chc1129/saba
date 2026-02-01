64aaa2d99cfee3c8c9f077e7dcf9f39972b0a207
chc1129 <chc1129@gmail.com>
2026-02-01 19:35:36 +0900

    chp3 ソケットアドレスの定義, ストリームの構築, リクエストラインの構築

--- net/wasabi/src/http.rs
+++ net/wasabi/src/http.rs
@@ -4,6 +4,8 @@ use alloc::string::String;
 use alloc::string::ToString;
 use allock::vec::Vec;
 use noli::net::lookup_host;
+use noli::net::SocketAddr;
+use noli::net::TcpStream;
 use saba_core::error::Error;
 use saba_core::http::HttpResponse;
 
@@ -28,5 +30,20 @@ impl HttpClient {
         if ips.len() < 1 {
             return Err(Error::Newtowrk("Failed to find IP addresses".to_string()));
         }
+
+        let socket_addr: SocketAddr = (ips[0], port).into();
+
+        let mut stream = match TcpStream::conect(socket_addr) {
+            Ok(stream) => stream,
+            Err(_) => {
+                return Err(Error::Network(
+                    "Failed to connect to TCP stream".to_string(),
+                ))
+            }
+        };
+
+        let mut request = String::from("GET /");
+        request.push_str(&path);
+        request.push_str(" HTTP/1.1\n");
     }
 }
