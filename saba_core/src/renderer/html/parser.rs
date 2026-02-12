3a6381235ad6d43d31895c5061d1351aad245219
chc1129 <chc1129@gmail.com>
2026-02-13 08:31:39 +0900

    chp4 要素ノードの追加 開いている要素のスタックの管理 テキストノードの追加(途中)

--- saba_core/src/renderer/html/parser.rs
+++ saba_core/src/renderer/html/parser.rs
@@ -45,6 +45,35 @@ impl HtmlParser {
         }
     }
 
+    fn contain_in_stack(&mut self, element_kind: ElementKind) -> bool {
+        for i in 0..self.stack_of_open_elements.len() {
+            if self.stack_of_open_elements[i].borrow().element_kind() == Some(element_kind) {
+                return true;
+            }
+        }
+
+        false
+    }
+
+    fn pop_untile(&mut self, element_kind: ElementKind) {
+        assert!(
+            self.contain_in_stack(element_kind),
+            "stack doesn't have an element {:?}",
+            element_kind,
+        );
+
+        loop {
+            let current = match self.stack_of_open_elements.pop() {
+                Some(n) => n,
+                None => return,
+            };
+
+            if current.borrow().element_kind() == Some(element_kind) {
+                return;
+            }
+        }
+    }
+
     fn pop_current_node(&mut self, element_kind: ElementKind) -> bool {
         let current = match self.stack_of_open_elements.last() {
             Some(n) => n,
@@ -57,6 +86,12 @@ impl HtmlParser {
         }
 
         false
+    }
+
+    fn create_char(&self, c: char) -> Node {
+        let mut s = String::new();
+        s.push(c);
+        Node::new(NodeKind::Text(s))
     }
 
     fn create_element(&self, tag: &str, attributes: Vec<Attriburte> -> Node {
