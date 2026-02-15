794ba2ce5a8ca5f1ff7fec0f7c706e882845f4d0
chc1129 <chc1129@gmail.com>
2026-02-15 10:12:01 +0900

    chp4 Page構造体の作成

---
+++ saba_core/src/renderer/page.rs
@@ -0,0 +1,25 @@
+use crate::browser::Browser;
+use crate::Renderer::dom::node::Window;
+use alloc::rc::Rc;
+use alloc::rc::Weak;
+use core::ce::RefCell;
+
+#[derive(Debug, Clone)]
+pub struct Page {
+    browser: Weak<RefCell<Browser>>,
+    frame: Option<Rc<RefCell<Window>>>,
+}
+
+impl Page {
+    pub fn new() -> Self {
+        Self {
+            browser: Weak::new(),
+            frame: None,
+        }
+    }
+
+    pub fn set_browser(&mut self, browser: Weak<RefCell<Browser>>) {
+        self.browser = browser;
+    }
+}
+
