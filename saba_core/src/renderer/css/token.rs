04c947ab7e33aae47525ce36e99a38f2e368c143
chc1129 <chc1129@gmail.com>
2026-02-15 16:16:22 +0900

    chp5 CSSで装飾する  ---次のトークンを返すメソッドの実装   (実施中)

---
+++ saba_core/src/renderer/css/token.rs
@@ -0,0 +1,58 @@
+use alloc::string::String;
+use alloc::vec::Vec;
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum CssToken {
+    /// https://www.w3.org/TR/css-syntax-3/#typedef-hash-token
+    HashToken(String),
+    /// https://www.w3.org/TR/css-syntax-3/#typedef-delim-token
+    Delim(char),
+    /// https://www.w3.org/TR/css-syntax-3/#typedef-number-token
+    Number(f64),
+    /// https://www.w3.org/TR/css-syntax-3/#typedef-colon-token
+    Colon,
+    /// https://www.w3.org/TR/css-syntax-3/#typedef-semicolon-token
+    SemiColon,
+    /// https://www.w3.org/TR/css-syntax-3/#tokendef-open-paren
+    OpenParenthesis,
+    /// https://www.w3.org/TR/css-syntax-3/#tokendef-close-paren
+    CloseParenthesis,
+    /// https://www.w3.org/TR/css-syntax-3/#tokendef-open-curly
+    OpenCurly,
+    /// https://www.w3.org/TR/css-syntax-3/#tokendef-close-curly
+    CloseCurly,
+    /// https://www.w3.org/TR/css-syntax-3/#typedef-ident-token
+    Ident(String),
+    /// https://www.w3.org/TR/css-syntax-3/#typedef-string-token
+    StringToken(String),
+    /// https://www.w3.org/TR/css-syntax-3/#typedef-at-keyword-token
+    AtKeyword(String),
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct CssTokenizer {
+    pos: usize,
+    input: Vec<char>,
+}
+
+impl Iterator for CssTokenizer {
+    type Item = CssToken;
+
+    /// https://www.w3.org/TR/css-syntax-3/#consume-token
+    fn next(&mut self) -> Option<Self::Item> {
+        loop {
+            if self.pos >= self.input.len() {
+                return None;
+            }
+
+            let c = self.input[sefl.pos];
+
+            let token = match c {
+                // 次のトークンを決定する
+            };
+
+            self.pos += 1;
+            return Some(token);
+        }
+    }
+}
