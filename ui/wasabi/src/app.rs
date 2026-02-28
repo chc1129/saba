d1c18f554e98c217f38b086d9fcc82cf154ca4de
chc1129 <chc1129@gmail.com>
2026-02-28 22:16:59 +0900

    chp6   ツールバーをクリックして入力を開始する   ～   マウスを描画する

--- ui/wasabi/src/app.rs
+++ ui/wasabi/src/app.rs
@@ -1,13 +1,13 @@
 use crate::alloc::string::ToString;
-
+use crate::cursor::Cursor;
 use alloc::format;
 use alloc::rc::Rc;
-
+use alloc::string::String;
 use core::cell::RefCell;
 use noli::error::Result as OsResult;
 use noli::prelude::SystemApi;
 use noli::println;
-
+use noli::rect::Rect;
 use noli::sys::api::MouseEvent;
 use noli::sys::wasabi::Api;
 use noli::window::StringSize;
@@ -21,14 +21,24 @@ use saba_core::constants::WINDOW_WIDTH;
 use saba_core::constants::*;
 
 use saba_core::error::Error;
+use saba_core::http::HttpResponse;
 
 
+
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
+enum InputMode {
+    Normal,
+    Editing,
+}
 
 
 #[derive(Debug)]
 pub struct WasabiUI {
     browser: Rc<RefCell<Browser>>,
+    input_url: String,
+    input_mode: InputMode,
     window: Window,
+    cursor: Cursor,
 }
 
 
@@ -37,6 +47,8 @@ impl WasabiUI {
     pub fn new(browser: Rc<RefCell<Browser>>) -> Self {
         Self {
             browser,
+            input_url: String::new(),
+            input_mode: InputMode::Normal,
             window: Window::new(
                 "saba".to_string(),
                 WHITE,
@@ -46,41 +58,99 @@ impl WasabiUI {
                 WINDOW_HEIGHT,
             )
             .unwrap(),
+            cursor: Cursor::new(),
         }
     }
 
 
 
 
-    pub fn start(&mut self) -> Result<(), Error> {
+    pub fn start(
+        &mut self,
+        handle_url: fn(String) -> Result<HttpResponse, Error>,
+    ) -> Result<(), Error> {
         self.setup()?;
 
-        self.run_app()?;
+        self.run_app(handle_url)?;
 
         Ok(())
     }
 
-    fn run_app(&mut self) -> Result<(), Error> {
+    fn run_app(
+        &mut self,
+        handle_url: fn(String) -> Result<HttpResponse, Error>,
+    ) -> Result<(), Error> {
         loop {
-            self.handle_mouse_input()?;
+            self.handle_mouse_input(handle_url)?;
             self.handle_key_input()?;
         }
     }
 
 
-    fn handle_mouse_input(&mut self) -> Result<(), Error> {
-       if let Some(MouseEvent { button, position }) = Api::get_mouse_cursor_info() {
-           println!("mouse position {:?}", position);
-           if button.l() || button.c() || button.r() {
-               println!("mouse clicked {:?}", button);
+    fn handle_mouse_input(
+        &mut self,
+        handle_url: fn(String) -> Result<HttpResponse, Error>,
+    )-> Result<(), Error> {
+        if let Some(MouseEvent { button, position }) = Api::get_mouse_cursor_info() {
+            self.window.flush_area(self.cursor.rect());
+            self.cursor.set_position(position.x, position.y);
+            self.window.flush_area(self.cursor.rect());
+            self.cursor.flush();
+
+            if button.l() || button.c() || button.r() {
+                // 相対位置を計算する
+                let relative_pos = (
+                    position.x - WINDOW_INIT_X_POS,
+                    position.y - WINDOW_INIT_Y_POS,
+                );
+
+                // ウィンドウの外をクリックされたときは何もしない
+                if relative_pos.0 < 0
+                    || relative_pos.0 > WINDOW_WIDTH
+                    || relative_pos.1 < 0
+                    || relative_pos.1 > WINDOW_HEIGHT
+                {
+                    println!("button clicked OUTSIDE window: {button:?} {position:?}");
+
+                    return Ok(());
+                }
+
+                // ツールバーの範囲をクリックされたとき、InputModeをEditingに変更する
+                if relative_pos.1 < TOOLBAR_HEIGHT + TITLE_BAR_HEIGHT
+                    && relative_pos.1 >= TITLE_BAR_HEIGHT
+                {
+                    self.clear_address_bar()?;
+                    self.input_url = String::new();
+                    self.input_mode = InputMode::Editing;
+                    println!("button clicked in toolbar: {button:?} {position:?}");
+                    return Ok(());
+                }
+
+                self.input_mode = InputMode::Normal;
            }
        }
        Ok(())
     }
 
     fn handle_key_input(&mut self) -> Result<(), Error> {
-        if let Some(c) = Api::read_key() {
-            println!("input text: {:?}", c);
+        match self.input_mode {
+            InputMode::Normal => {
+                // InputModeがNormalのとき、キー入力を無視する
+                let _ = Api::read_key();
+            }
+            InputMode::Editing => {
+                if let Some(c) = Api::read_key() {
+                    if c == 0x7F as char || c == 0x08 as char {
+                        // デリートキーまたはバックスペースキーが押されたので、
+                        // 最後の文字を削除する
+                        self.input_url.pop();
+                        self.update_address_bar()?;
+                    } else {
+                        self.input_url.push(c);
+                        self.update_address_bar()?;
+                    }
+                }
+            }
         }
 
         Ok(())
@@ -142,7 +212,81 @@ impl WasabiUI {
         Ok(())
     }
 
+    fn update_address_bar(&mut self) -> Result<(), Error> {
+        // アドレスバーを白く塗り潰す
+        if self
+            .window
+            .fill_rect(WHITE, 72, 4, WINDOW_WIDTH - 76, ADDRESSBAR_HEIGHT - 2)
+            .is_err()
+        {
+            return Err(Error::InvalidUI(
+                    "failed to clear an address bar".to_string(),
+            ));
+        }
+
+        // input_urlをアドレスバーに描画する
+        if self
+            .window
+            .draw_string(
+                BLACK,
+                74,
+                6,
+                &self.input_url,
+                StringSize::Medium,
+                /*underline=*/ false,
+            )
+            .is_err()
+        {
+            return Err(Error::InvalidUI(
+                    "failed to update an address bar".to_string(),
+            ));
+        }
+
+        // アドレスバーの部分の画面を更新する
+        self.window.flush_area(
+            Rect::new(
+                WINDOW_INIT_X_POS,
+                WINDOW_INIT_Y_POS + TITLE_BAR_HEIGHT,
+                WINDOW_WIDTH,
+                TOOLBAR_HEIGHT,
+            )
+            .expect("failed to create a rect for the address bar"),
+        );
+
+        Ok(())
+    }
+
+    fn clear_address_bar(&mut self) -> Result<(), Error> {
+        // アドレスバーを白く塗りつぶす
+        if self
+            .window
+            .fill_rect(WHITE, 72, 4, WINDOW_WIDTH - 76, ADDRESSBAR_HEIGHT - 2)
+            .is_err()
+        {
+            return Err(Error::InvalidUI(
+                    "failed to clear an address bar".to_string(),
+            ));
+        }
+
+        // アドレスバーの部分の画面を更新する
+        self.window.flush_area(
+            Rect::new(
+                WINDOW_INIT_X_POS,
+                WINDOW_INIT_Y_POS + TITLE_BAR_HEIGHT,
+                WINDOW_WIDTH,
+                TOOLBAR_HEIGHT,
+            )
+            .expect("failed to create a rect for the address bar"),
+        );
+
+        Ok(())
+    }
+
+
 
 
 }
+
+
+
 
